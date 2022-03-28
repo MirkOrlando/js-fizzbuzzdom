cef2e55ad4d10f14c1bcadc9bb649e0924a08ca8
MirkOrlando <mirko@orlando-m.it>
2022-03-28 14:56:59 +0200

    created fizzBuzzElement and wrote it on console

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,5 +7,18 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 /* creare il ciclo per stampare i numeri da 1 a 100 */
 
 for (let i = 1; i <= 100; i++) {
-  console.log(i);
+  let fizzBuzzElement = i;
+  //console.log(i);
+
+  /* verificare se i è multipo di 3 o di 5 o di 3 e 5 */
+  if (fizzBuzzElement % 3 == 0 && fizzBuzzElement % 5 == 0) {
+    fizzBuzzElement = "FizzBuzz";
+  } else if (fizzBuzzElement % 5 == 0) {
+    fizzBuzzElement = "Buzz";
+  } else if (fizzBuzzElement % 3 == 0) {
+    fizzBuzzElement = "Fizz";
+  }
+
+  /* stampare il risultato in console*/
+  console.log(fizzBuzzElement);
 }
