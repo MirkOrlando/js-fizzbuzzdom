6cc6d77194c295aee049e9f040928b3e03879f3a
MirkOrlando <mirko@orlando-m.it>
2022-03-28 16:51:08 +0200

    added layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,18 +11,22 @@ for (let i = 1; i <= 100; i++) {
   let fizzBuzzElement = i;
   //console.log(i);
   const squareElement = document.createElement("div");
+  squareElement.classList.add("square");
+  squareElement.classList.add("bg-primary");
 
   /* verificare se i è multipo di 3 o di 5 o di 3 e 5 */
   if (fizzBuzzElement % 3 == 0 && fizzBuzzElement % 5 == 0) {
     fizzBuzzElement = "FizzBuzz";
+    squareElement.classList.add("bg-danger");
   } else if (fizzBuzzElement % 5 == 0) {
     fizzBuzzElement = "Buzz";
+    squareElement.classList.add("bg-warning");
   } else if (fizzBuzzElement % 3 == 0) {
     fizzBuzzElement = "Fizz";
+    squareElement.classList.add("bg-success");
   }
 
   /* stampare il risultato*/
   //console.log(fizzBuzzElement);
-  squareElement.classList.add("square");
   squareContainerElement.appendChild(squareElement).innerHTML = fizzBuzzElement;
 }
