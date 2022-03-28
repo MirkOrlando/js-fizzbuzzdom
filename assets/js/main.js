4722970edb8b26c3e0af4c59fce9739fc9544a10
MirkOrlando <mirko@orlando-m.it>
2022-03-28 14:19:48 +0200

    created for-loop numbers from 1 to 100

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,9 @@ Per i multipli di 3 stampi “Fizz” al posto del numero
 Per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 */
+
+/* creare il ciclo per stampare i numeri da 1 a 100 */
+
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
